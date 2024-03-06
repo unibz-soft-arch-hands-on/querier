301540d411ea65d48b422513c3f6da653faaaa5b
João Daniel <jotaf.daniel@gmail.com>
2024-03-06 14:16:50 +0100

    unzip start.spring.io package
    https://start.spring.io/#!type=gradle-project-kotlin&language=java&platformVersion=3.2.3&packaging=jar&jvmVersion=17&groupId=it.unibz.bulletify&artifactId=querier&name=querier&description=A%20demo%20for%20layered%20and%20microservices%20architectures&packageName=it.unibz.bulletify.querier&dependencies=web,data-jpa,h2,postgresql,amqp,amqp-streams

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.3"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "it.unibz.bulletify"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-amqp")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.amqp:spring-rabbit-stream")
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.amqp:spring-rabbit-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/java/it/unibz/bulletify/querier/QuerierApplication.java
@@ -0,0 +1,13 @@
+package it.unibz.bulletify.querier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerierApplication.class, args);
+	}
+
+}
